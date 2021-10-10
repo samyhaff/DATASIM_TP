4f91fdcd7463d827a347ad5e4f339ca55675b591
Samy Haffoudhi <haffoudhisamy@gmail.com>
2021-10-10 21:52:51 +0200

    avancee

--- ARSIG/TP2/ex2.m
+++ ARSIG/TP2/ex2.m
@@ -3,18 +3,26 @@ addpath(genpath('tftb-0.2'));
 [x, Fs] = audioread('furElise_court.wav');
 
 N = length(x);
+t = 0:1/Fs:(N-1)/Fs;
+
+% subplot(2,1,1);
+% plot(t,x);
+% title('Allure temporelle')
+% xlabel('temps (s)')
+% xlim([0 t(N)])
+% ylabel('Amplitude')
 
 Nh = 513;
 Nf = 8*2^nextpow2(Nh);
-
-subplot(2,1,1);
-plot(x);
-
 h = hamming(Nh);
 [tfrx,T,F] = tftb_spectrogram(x',Nf,h);
+
 subplot(2, 1, 2);
 imagesc(T,F,log(tfrx)); axis xy;
 colorbar;
+title('Spectrogramme en échelle logarithmique')
+xlabel('n')
+ylabel('Fréquence normalisée')
 
 tau = 10;
 seuil = 0.1;
@@ -28,10 +36,9 @@ for t = tau+1:N-tau
     I(t) = sum(abs(R1 - R2));
 end
 
-% I(I < seuil) = 0;
 [maxima, ruptures] = findpeaks(I,'MinPeakHeight',0.04,'MinPeakProminence',0.003);
 Nf=2^nextpow2(N);
-freq = 0:Fs/Nf:(Nf-1)/Nf*Fs;
+axe_freq=linspace(0,Fs-Fs/Nf,Nf);
 
 notes = [];
 durees = [];
@@ -40,15 +47,14 @@ for i = 1:length(ruptures)-1
     x_plage = x(ruptures(i):ruptures(i+1));
     TF = fft(x_plage,Nf);
     TF = abs(TF).^2;
-    axe_freq=linspace(0,Fs-Fs/Nf,Nf);
     [maxima, lambda] = max(TF);
     f = axe_freq(lambda);
     ecart = tab_freq_valeurs - f;
     [minimum, indice] = min(abs(ecart));
     notes = [notes; tab_freq_noms(indice,:)];
-    nb_points =ruptures(i+1)-ruptures(i);
-    durees=[durees; nb_points/Fs];
+    nb_points = ruptures(i+1)-ruptures(i);
+    durees = [durees; nb_points/Fs];
 end
 
-s=genere_morceau(notes,durees,Fs);
+s = genere_morceau(notes,durees,Fs);
 audiowrite('eliseTest.wav', s, Fs);
