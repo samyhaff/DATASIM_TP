2aa6696e29e589a5fe725d7e7d6f5ab81f41c60c
Samy Haffoudhi <haffoudhisamy@gmail.com>
2021-11-02 20:24:04 +0100

    fin TP

--- CSOPT/TP1/critere_debruitage.m
+++ CSOPT/TP1/critere_debruitage.m
@@ -2,9 +2,9 @@ function [f,g,h,J] = critere_debruitage(x, params)
     f0 = norm(params.y - x) ^ 2;
     n = length(x);
     D = diag(ones(n, 1)) - diag(ones(n-1,1),-1);
-    f1 = norm(D * x')^2;
+    f1 = norm(D * x)^2;
     f = (1 - params.lambda) * f0 + params.lambda * f1;
-    g = -2 * (1 - params.lambda) * (params.y - x)' + 2 * params.lambda * D' * D * x';
+    g = -2 * (1 - params.lambda) * (params.y - x) + 2 * params.lambda * D' * D * x;
     h = 2 * (1 - params.lambda) + 2 * params.lambda * D' * D;
-    J = 0; % à changer
+    J = [];
 end
--- CSOPT/TP1/ex3.m
+++ CSOPT/TP1/ex3.m
@@ -1,31 +1,79 @@
 close all
 clear all
 
-n = 1000;
+n = 100;
 
 x = simsignal(n, 'spectre');
+
+figure(1)
+plot(x)
+title('Signal de mesure')
+xlabel('n')
+ylabel('Amplitude')
 
 E = std(x)^2;
 variance = E / 100;
 b = sqrt(variance)*randn([1, n]);
-
 y = x + b;
 
-plot(y, 'r');
-hold on
-plot(x, 'b');   
+figure(2)
+plot(y);
+title('Signal de mesure en présence de bruit')
+xlabel('n')
+ylabel('Amplitude')
 
-lambda = 0.5;
-D = diag(ones(n, 1)) - diag(ones(n-1,1),-1);
-K=eye(size(y));
-Sfinal=(inv((1-lambda)*K'*K+lambda*D'*D))*(1-lambda)*K'.*y';
-plot(Sfinal);
-err=[];
-pas = 0.01;
-for lambda=0:pas:1
-    S=inv((1-lambda)*K'*K+lambda*D'*D)*(1-lambda)*K'.*y';
-    err = [err;norm(S-x)];
+options.maxiter = 10 ^ 3;
+options.tolX = 10 ^ (-4);
+options.tolG = 10 ^ (-4);
+options.tolF = 10 ^ (-4);
+options.method='gradient';
+options.pas='variable';
+options.pasInit = 0.01;
+options.beta = 0.75;
+options.const = 10 ^ (-4);
+
+critfun = 'critere_debruitage';
+params.y = y';
+params.lambda = 0.5;
+x0 = y';
+
+[xh,result,xval] = optimdescent(critfun,params,options,x0);
+
+figure(3)
+plot(xh)
+hold on
+plot(x)
+plot(y)
+legend('signal estimé','signal original','signal bruité')
+
+lambdas = 0:0.05:1;
+err = zeros(1,length(lambdas));
+xmin = zeros(n,length(lambdas));
+for i = 1:length(lambdas)
+    params.lambda = lambdas(i);
+    [xh,result,xval] = optimdescent(critfun,params,options,x0);
+    err(i) = sqrt(sum((x'-xh).^2));
+    xmin(:,i) = xh;
 end
-lambda=0:pas:1;
-figure;
-plot(lambda,err);
+
+figure(4)
+plot(lambdas, err)
+title('Erreur en fonction de lambda')
+xlabel('lambda')
+ylabel('Erreur')
+
+f0 = zeros(1,length(lambdas));
+f1 = zeros(1,length(lambdas));
+D = diag(ones(n, 1)) - diag(ones(n-1,1),-1);
+for i = 1:length(lambdas)
+    f0(i) = norm(params.y - xmin(:,i)) ^ 2;
+    f1(i) = norm(D * xmin(:,i))^2;
+end
+
+figure(5)
+plot(f1, f0)
+title('f0 en fonction de f1')
+xlabel('f1')
+ylabel('f0')
+hold on
+plot(1:length(lambdas),1:length(lambdas))
